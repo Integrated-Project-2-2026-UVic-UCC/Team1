57d0c590a8134606876831977e1b048f7355a22e
nllortfer <nllortfer@gmail.com>
2026-02-23 21:51:32 +0100

    added varaible for each motor leg, and loop is always moving servos

--- Software/firmware/embedded-leg-ctrl/src/main.cpp
+++ Software/firmware/embedded-leg-ctrl/src/main.cpp
@@ -26,24 +26,54 @@ z_owned_session_t s;      // session object
 z_owned_subscriber_t sub; // subsciber object
 
 uint8_t buffer[256]; // creem buffer de 1 byte
-float leg1[3];
-float leg2[3];
-float leg3[3];
-float leg4[3];
+float leg_lf[3];
+float leg_rf[3];
+float leg_lh[3];
+float leg_rh[3];
 
 #define LED_PIN 2 // Pin GPIO
+
 #define LF_HHA 16 // left front hip abductor adductor
 #define LF_HFE 17 // left front hip flexor extensor
 #define LF_KFE 18 // left front knee flexor extensor
+#define RF_HHA 19 // right front hip abductor adductor
+#define RF_HFE 21 // right front hip flexor extensor
+#define RF_KFE 22 // right front knee flexor extensor
+#define LH_HHA 23 // left hind hip abductor adductor
+#define LH_HFE 25 // left hind hip flexor extensor
+#define LH_KFE 26 // left hind knee flexor extensor
+#define RH_HHA 27 // right hind hip abductor adductor
+#define RH_HFE 32 // right hind hip flexor extensor
+#define RH_KFE 33 // right hind knee flexor extensor
 
-const int lf_haa_channel = 0; // Canal LEDC (0-15)
-const int lf_hfe_channel = 1; // Canal LEDC (0-15)
-const int lf_kfe_channel = 2; // Canal LEDC (0-15)
-const int freq = 50;          // Frecuencia PWM
-const int resolution = 16;    // Resolución (1-16 bits)
+const int lf_haa_channel = 0;  // Canal LEDC (0-15)
+const int lf_hfe_channel = 1;  // Canal LEDC (0-15)
+const int lf_kfe_channel = 2;  // Canal LEDC (0-15)
+const int rf_haa_channel = 3;  // Canal LEDC (0-15)
+const int rf_hfe_channel = 4;  // Canal LEDC (0-15)
+const int rf_kfe_channel = 5;  // Canal LEDC (0-15)
+const int lh_haa_channel = 6;  // Canal LEDC (0-15)
+const int lh_hfe_channel = 7;  // Canal LEDC (0-15)
+const int lh_kfe_channel = 8;  // Canal LEDC (0-15)
+const int rh_haa_channel = 9;  // Canal LEDC (0-15)
+const int rh_hfe_channel = 10; // Canal LEDC (0-15)
+const int rh_kfe_channel = 11; // Canal LEDC (0-15)
+
+const int freq = 50;       // Frecuencia PWM
+const int resolution = 16; // Resolución (1-16 bits)
 
 ServoCtrl lf_haa(LF_HHA, lf_haa_channel, resolution);
 ServoCtrl lf_hfe(LF_HFE, lf_hfe_channel, resolution);
+ServoCtrl lf_kfe(LF_KFE, lf_kfe_channel, resolution);
+ServoCtrl rf_haa(RF_HHA, rf_haa_channel, resolution);
+ServoCtrl rf_hfe(RF_HFE, rf_hfe_channel, resolution);
+ServoCtrl rf_kfe(RF_KFE, rf_kfe_channel, resolution);
+ServoCtrl lh_haa(LH_HHA, lh_haa_channel, resolution);
+ServoCtrl lh_hfe(LH_HFE, lh_hfe_channel, resolution);
+ServoCtrl lh_kfe(LH_KFE, lh_kfe_channel, resolution);
+ServoCtrl rh_haa(RH_HHA, rh_haa_channel, resolution);
+ServoCtrl rh_hfe(RH_HFE, rh_hfe_channel, resolution);
+ServoCtrl rh_kfe(RH_KFE, rh_kfe_channel, resolution);
 
 void data_handler(z_loaned_sample_t *sample, void *arg)
 {
@@ -55,16 +85,18 @@ void data_handler(z_loaned_sample_t *sample, void *arg)
     len = sizeof(buffer);
   z_bytes_reader_read(&reader, buffer, len);
   // Imprime todo en hex para ver la estructura
-  leg1[0] = *(float *)(&buffer[16]);
-  leg1[1] = *(float *)(&buffer[20]);
-  leg1[2] = *(float *)(&buffer[24]);
-
-  Serial.print("Leg 1: ");
-  Serial.print(leg1[0]);
-  Serial.print(", ");
-  Serial.print(leg1[1]);
-  Serial.print(", ");
-  Serial.println(leg1[2]);
+  leg_lf[0] = *(float *)(&buffer[16]);
+  leg_lf[1] = *(float *)(&buffer[20]);
+  leg_lf[2] = *(float *)(&buffer[24]);
+  leg_rf[0] = *(float *)(&buffer[28]);
+  leg_rf[1] = *(float *)(&buffer[32]);
+  leg_rf[2] = *(float *)(&buffer[36]);
+  leg_lh[0] = *(float *)(&buffer[40]);
+  leg_lh[1] = *(float *)(&buffer[44]);
+  leg_lh[2] = *(float *)(&buffer[48]);
+  leg_rh[0] = *(float *)(&buffer[52]);
+  leg_rh[1] = *(float *)(&buffer[56]);
+  leg_rh[2] = *(float *)(&buffer[60]);
 }
 
 void setup()
@@ -147,8 +179,17 @@ void setup()
 
 void loop()
 {
-  lf_haa.write(leg1[0]);
-  lf_hfe.write(leg1[1]);
-  // lf_kfe.write(leg1[2]);
+  lf_haa.write(leg_lf[0]);
+  lf_hfe.write(leg_lf[1]);
+  lf_kfe.write(leg_lf[2]);
+  rf_haa.write(leg_rf[0]);
+  rf_hfe.write(leg_rf[1]);
+  rf_kfe.write(leg_rf[2]);
+  lh_haa.write(leg_lh[0]);
+  lh_hfe.write(leg_lh[1]);
+  lh_kfe.write(leg_lh[2]);
+  rh_haa.write(leg_rh[0]);
+  rh_hfe.write(leg_rh[1]);
+  rh_kfe.write(leg_rh[2]);
 }
 #endif
