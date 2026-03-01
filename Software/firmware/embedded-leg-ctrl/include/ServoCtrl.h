ddd2f77a1bab3a47fd8e995171570596ba835c6b
nllortfer <nllortfer@gmail.com>
2026-03-01 20:38:06 +0100

    Added namespaces for pwm params and limit functions

--- Software/firmware/embedded-leg-ctrl/include/ServoCtrl.h
+++ Software/firmware/embedded-leg-ctrl/include/ServoCtrl.h
@@ -1,7 +1,16 @@
 #ifndef SERVO_CTRL_H
 #define SERVO_CTRL_H
 #include <Arduino.h>
-#include <math.h> // Corregido: es math.h, no mat.h
+#include <math.h>
+
+namespace ServoCfg // constexpr: se evalua en tiempo de compliacion, no ocupa RAM, no cambian en ejecucion
+{                  // static: en librerias, al no cambiar en cada definicion, se comparte entre todas las instancias, no ocupa RAM extra por cada instancia
+    static constexpr float freq = 50.0;
+    static constexpr float min_us = 500.0;
+    static constexpr float max_us = 2500.0;
+    static constexpr float period_us = 20000.0;               // 50 Hz
+    static constexpr float abs_max_rads = (3.0 * M_PI) / 2.0; // 270 grados
+}
 
 class ServoCtrl
 {
@@ -9,11 +18,13 @@ public:
     ServoCtrl(int pin, int channel, int resolution);
     void write(float rads);
     void writeDegrees(float degrees);
+    void setLimit(float max_rads); // method to set maximum values to no break the leg
 
 private:
     int _pin;
     int _channel;
     int _resolution;
+    float _max_rads;
 };
 
 ServoCtrl::ServoCtrl(int pin, int channel, int resolution)
@@ -21,31 +32,30 @@ ServoCtrl::ServoCtrl(int pin, int channel, int resolution)
     _pin = pin;
     _channel = channel;
     _resolution = resolution;
-    ledcSetup(_channel, 50, _resolution);
+    _max_rads = ServoCfg::abs_max_rads; // robot maximum range is 270 //0.09 margin
+    ledcSetup(_channel, ServoCfg::freq, _resolution);
     ledcAttachPin(_pin, _channel);
+}
+
+void ServoCtrl::setLimit(float max_rads)
+{
+    _max_rads = max_rads;
 }
 
 void ServoCtrl::write(float rads)
 {
-    const float min_us = 500.0;
-    const float max_us = 2500.0;
-    const float period_us = 20000.0; // 50 Hz
-
-    const float max_servo_rads = (3.0 * M_PI) / 2.0 + 0.09; // aprox 4.71 rads mes offset de tolerancies
-
-    // capar rangos
+    if (rads > _max_rads)
+        rads = _max_rads;
     if (rads < 0)
         rads = 0;
-    if (rads > max_servo_rads)
-        rads = max_servo_rads;
 
     // TODO: añadir offset por rangos
 
     // FÓRMULA CORREGIDA:
     // Calculamos el pulso basándonos en que el máximo recorrido es 270 grados
-    float pulse = min_us + (rads / max_servo_rads) * (max_us - min_us);
+    float pulse = ServoCfg::min_us + (rads / ServoCfg::abs_max_rads) * (ServoCfg::max_us - ServoCfg::min_us);
 
-    float duty = pulse / period_us;
+    float duty = pulse / ServoCfg::period_us;
 
     // Cálculo a 16 bits seguro
     uint16_t max_duty_val = (1 << _resolution) - 1;
